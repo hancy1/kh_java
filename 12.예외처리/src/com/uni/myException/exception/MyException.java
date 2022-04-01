720af66ef8cb8e6beb20556dd7789351c97cd404
codud <codud@192.168.168.110>
2022-04-01 22:11:19 +0900

    12.예외처리

---
+++ 12.예외처리/src/com/uni/myException/exception/MyException.java
@@ -0,0 +1,14 @@
+package com.uni.myException.exception;
+
+public class MyException extends Exception {
+
+	public MyException() {
+	}
+
+	public MyException(String message) {
+		super(message);
+		
+	}
+
+	
+}
