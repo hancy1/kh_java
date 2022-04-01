6e4315dd689062209ff637bffc1196611cdd3be4
codud <codud@192.168.168.110>
2022-04-01 23:22:25 +0900

    JDBC_01_Statement

---
+++ 01_Statement/src/com/uni/member/run/Run.java
@@ -0,0 +1,12 @@
+package com.uni.member.run;
+
+import com.uni.member.view.MemberMenu;
+
+public class Run {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new MemberMenu().mainMenu();
+	}
+
+}
