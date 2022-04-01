8e6b7b72d5b388e91eb6baf8a41a3c9f556b6a6b
codud <codud@192.168.168.110>
2022-04-01 23:22:57 +0900

    JDBC_03_Singleton_Service

---
+++ 03_Singleton_Service/src/com/uni/member/run/Run.java
@@ -0,0 +1,13 @@
+package com.uni.member.run;
+
+import com.uni.member.view.MemberMenu;
+
+public class Run {
+
+	public static void main(String[] args) {
+
+		new MemberMenu().mainMenu();
+
+	}
+
+}
